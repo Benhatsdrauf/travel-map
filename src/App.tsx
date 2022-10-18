b969ee2168480342e6d9c51a83f4b1ae894e5824
Benjamin Polin <57519356+Benhatsdrauf@users.noreply.github.com>
2022-10-18 17:24:32 +0200

    lets go :D

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,8 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <h1>This is the start of something cool ... i hope :D</h1>
     </div>
   );
 }
