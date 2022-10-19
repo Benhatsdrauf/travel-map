d8dab9c4d1e7f4c8d668dae26033ed7c3dcfacf6
Benjamin Polin <bpolin@consolidate.eu>
2022-10-19 08:36:35 +0200

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { useState } from "react";
-
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
     <div>
       <h1>This is the start of something cool ... i hope :D</h1>
     </div>
   );
-}
+};
 
 export default App;
